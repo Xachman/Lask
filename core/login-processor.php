69c7794b8a27eb49b19485107735918aa0fdd69b
Zach Ironside <zironside@hotmail.com>
2015-09-15 07:03:25 -0400

    updated login class and added functions

--- core/login-processor.php
+++ core/login-processor.php
@@ -1,7 +1,7 @@
 <?php
-include($sc->rootURL.'functions.php');
+include($sc->rootURL.'inc/class-login.php');
 if (isset($_POST['email'])){
-	include_once('inc/db_conx.php');
+	include_once($sc->rootURL.'inc/db_conx.php');
 
 	$e = mysqli_real_escape_string($db_conx, $_POST['email']);
 	$p = $_POST['password'];
@@ -10,29 +10,29 @@ if (isset($_POST['email'])){
 
 	if($e == "" || $p == "") {
 		echo "login failed";
-		failed_login($db_conx, $e, $ip);
+		$login->failed_login($db_conx, $e, $ip);
 		exit();
 	} else {
 		$row= $db->select_row("SELECT id, username, password, level FROM users WHERE email='$e' LIMIT 1");
 		$db_id = $row[0];
 		$db_username = $row[1];
 		$db_pass_str = $row[2];
-		if (!check_pass($p, $db_pass_str)) {
+		if (!$login->check_pass($p, $db_pass_str)) {
 			echo "login failed";
-			failed_login($db_conx, $e, $ip);
+			$login->failed_login($db_conx, $e, $ip);
 			exit();
 		} else {
-			login_success($db_conx, $e, $ip);
+			$login->login_success($db_conx, $e, $ip);
 			$_SESSION['userid'] = $db_id;
 			$_SESSION['username'] = $db_username;
 			$_SESSION['password'] = $db_pass_str;
 			setcookie("id", $db_id, strtotime('+30 days'), "/", "", "", TRUE);
 			setcookie("id", $db_username, strtotime('+30 days'), "/", "", "", TRUE);
-			setcookie("id", $db_pass_str, strtotime('+30 days'), "/", "", "", TRUE);		
+			setcookie("id", $db_pass_str, strtotime('+30 days'), "/", "", "", TRUE);
 			$sql = "UPDATE users SET ip='$ip', lastlogin=now() WHERE username='$db_username' LIMIT 1";
 			$query = mysqli_query($db_conx, $sql);
 			echo 'success';
-			exit();	
+			exit();
 		}
 	}
 	exit();
--- inc/class-login.php
+++ inc/class-login.php
@@ -1,23 +1,45 @@
 <?php
-class login {
+include($sc->rootURL.'inc/db_conx.php');
+class Login {
+  private $db;
+  function __construct($db) {
+      $this->db = $db;
+  }
   public function failed_login($conx, $e, $ip) {
-  	$sql = "UPDATE login_attempts SET attempts=attempts + 1, username='$e' WHERE ip='$ip' LIMIT 1";
-  	$query = mysqli_query($conx, $sql);
+    //var_dump($this->db->query_row("UPDATE login_attempts SET attempts=attempts + 1, username='$e' WHERE ip='$ip' LIMIT 1"));
+  	if(!$this->db->query_row("UPDATE login_attempts SET attempts=attempts + 1, username='$e' WHERE ip='$ip' LIMIT 1")){
+      $this->db->insert_row("INSERT INTO login_attempts (ip, attempts, username) VALUES ('$ip', 1, '$e')");
+    }
+
   }
 
   public function login_success($conx, $e, $ip) {
-  	$sql = "UPDATE login_attempts SET attempts=0, username='$e' WHERE ip='$ip' LIMIT 1";
-  	$query = mysqli_query($conx, $sql);
+  	$this->db->query_row("UPDATE login_attempts SET attempts=0, username='$e' WHERE ip='$ip' LIMIT 1");
   }
   public function set_throttle($conx, $e, $ip, $time) {
   	if(!isset($time)){
   		$time = strtotime('5 minutes');
   	}
-  	$sql = "UPDATE login_attempts SET throttle='$time' WHERE ip='$ip' LIMIT 1";
-  	$query = mysqli_query($conx, $sql);
+  	$this->db->query_row("UPDATE login_attempts SET throttle='$time' WHERE ip='$ip' LIMIT 1");
   }
   public function adjust_attempts($conx, $e, $ip, $a) {
-  	$sql = "UPDATE login_attempts SET attempts=$a WHERE ip='$ip' LIMIT 1";
-  	$query = mysqli_query($conx, $sql);
+  	$this->db->query_row("UPDATE login_attempts SET attempts=$a WHERE ip='$ip' LIMIT 1");
+  }
+  public function crypt_pass($input, $rounds = 10) {
+  	$salt = "";
+  	$saltChars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+  	for($i = 0; $i < 22; $i++){
+  		$salt .= $saltChars[array_rand($saltChars)];
+  	}
+  	return crypt($input, sprintf('$2y$%02d$', $rounds) . $salt);
+  }
+
+  public function check_pass($input, $hp) {
+  	if(crypt($input, $hp) === $hp){
+  		return true;
+  	}else {
+  		return false;
+  	}
   }
 }
+$login = new Login($db);
